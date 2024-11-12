14aae34b7b9089dfcd4cdf30657420bfa54c19c7
Floor-board <Sterling34@duck.com>
2024-11-12 13:18:38 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, render_template, flash
+from markupsafe import Markup
+from flask import redirect
+from flask import session
+import os
 
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app.secret_key=os.environ["SECRET_KEY"]; #This is a variable.
+
+@app.route("/")
+def render_main():
+    return render_template('index.html')
+#@app.route("/p1")
+#def render_page1():
+ #   return render_template('page1.html')
+
+@app.route('/startOver')
+def startOver():
+    session.clear() #clears variable values and creates a new session
+    return redirect('/')
+if __name__=="__main__":
+    app.run(debug=False)
